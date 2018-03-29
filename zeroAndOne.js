26cc71b6fadf3a2afe74d08fa6a629b93717b472
edson <edson.batista@catho.com>
2018-03-29 18:44:46 -0300

    zeroAndOne.js

--- zeroAndOne.js
+++ zeroAndOne.js
@@ -11,14 +11,23 @@
     return vetorResultado;
 }*/
 
-var zeroAndOne = vetor =>{
-  
+var zeroAndOne = vetor =>{        
+    var resultado = vetor;
+    var inicio = 0
+    var final = (vetor.length - 1)
 
-    for(var i = 0; i < vetor.length; i++){
-       
+    for(var i = 0; i < vetor.length; i ++){
+
+        if(vetor[i] === 0){
+            resultado[inicio] = vetor[i];
+            inicio++; 
+        }else{
+            resultado[final]=vetor[i];
+            final--;
+        }
     }
 
-    return vetorOrdenado;
+    return resultado;
 
 }
 
@@ -26,4 +35,16 @@ var teste = [1,0,0,0,1,1,0,1];
 var teste2 = [1,0];
 var teste3 = [0,0,0,0,1,0,0];
 
-console.log(zeroAndOne(teste));
+console.log(zeroAndOne(teste3));
+
+/*    var array1 = [];
+    var array2 = [];     
+
+    for(var i = 0; i < vetor.length; i++){
+        if(vetor[i] === 0)
+            array1.push(vetor[i]);
+        else
+            array2.push(vetor[i]);           
+    }
+    
+    return ;*/ 
